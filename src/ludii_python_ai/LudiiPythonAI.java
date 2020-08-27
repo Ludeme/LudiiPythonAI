8a776c409a60ecdc26fcc72ace3a104a0b279508
Dennis Soemers <d.soemers@gmail.com>
2020-08-27 12:42:29 +0200

    initial setup Java code

---
+++ src/ludii_python_ai/LudiiPythonAI.java
@@ -0,0 +1,66 @@
+package ludii_python_ai;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+import game.Game;
+import main.collections.FastArrayList;
+import util.AI;
+import util.Context;
+import util.Move;
+import utils.AIUtils;
+
+/**
+ * Example Java wrapper for a Ludii AI implemented in Python
+ *
+ * @author Dennis Soemers
+ */
+public class LudiiPythonAI extends AI
+{
+	
+	//-------------------------------------------------------------------------
+	
+	/** Our player index */
+	protected int player = -1;
+	
+	//-------------------------------------------------------------------------
+	
+	/**
+	 * Constructor
+	 */
+	public LudiiPythonAI()
+	{
+		this.friendlyName = "Example Ludii Python AI";
+	}
+	
+	//-------------------------------------------------------------------------
+
+	@Override
+	public Move selectAction
+	(
+		final Game game, 
+		final Context context, 
+		final double maxSeconds,
+		final int maxIterations,
+		final int maxDepth
+	)
+	{
+		FastArrayList<Move> legalMoves = game.moves(context).moves();
+		
+		// If we're playing a simultaneous-move game, some of the legal moves may be 
+		// for different players. Extract only the ones that we can choose.
+		if (!game.isAlternatingMoveGame())
+			legalMoves = AIUtils.extractMovesForMover(legalMoves, player);
+		
+		final int r = ThreadLocalRandom.current().nextInt(legalMoves.size());
+		return legalMoves.get(r);
+	}
+	
+	@Override
+	public void initAI(final Game game, final int playerID)
+	{
+		this.player = playerID;
+	}
+	
+	//-------------------------------------------------------------------------
+
+}
